36be298f5f40f7ea449fbb6eb1010a56436fe11d
Xacone <benjamaayazid21@gmail.com>
2023-02-14 09:53:26 +0100

    Fix of the little concatenation problem

--- src/main/java/com/treatmentunit/restservice/APIController.java
+++ src/main/java/com/treatmentunit/restservice/APIController.java
@@ -1,20 +1,40 @@
 package com.treatmentunit.restservice;
 
+import com.treatmentunit.simulation.OptimisationAndFormating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class APIController {
 
     private static final String template = "Hello %s !";
     private static final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/wesh")
-    public String greetin(@RequestParam(value = "id") int id) {
-        return "On the rockzz " + id;
+    OptimisationAndFormating optAndForm = new OptimisationAndFormating();
+
+    @GetMapping("/test")
+    public String greetin(@RequestParam(value = "id") int id) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = Files.newBufferedReader(Paths.get("C:\\Users\\1234Y\\OneDrive\\Documents\\DossierEtudes2\\PROJ-SI\\rest-service\\rest-service\\src\\main\\java\\com\\treatmentunit\\restservice\\coords_to_test.txt"));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        //OptimisationAndFormating.FormatingAndInversing(sb.toString());
+        return optAndForm.getOutput(sb.toString());
+        //return optAndForm.getOutput(sb.toString());
     }
 
     /*
--- src/main/java/com/treatmentunit/simulation/OptimisationAndFormating.java
+++ src/main/java/com/treatmentunit/simulation/OptimisationAndFormating.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class OptimisationAndFormating {
 
+    private static ArrayList<String> arrangedCoords = new ArrayList<>();
+
     /**
      *
      * @param a -> Lat1 a
@@ -12,7 +14,7 @@ public class OptimisationAndFormating {
      * @param d -> Long2 d
      * @return distance en mètres
      */
-    public static double distanceViaLatEtLong(double a, double b, double c, double d) {
+    private static double distanceViaLatEtLong(double a, double b, double c, double d) {
         double resultat = 0;
         double R = 6371*1000;
         double phi_a_rad = a*(Math.PI/180);
@@ -30,28 +32,54 @@ public class OptimisationAndFormating {
     }
 
     public static ArrayList<String> FormatingAndInversing(String input) {
-        ArrayList<String> arrangedCoords = new ArrayList<>();
 
-        double lastPoint_lat = 0;
-        double lastPont_long = 0;
+        if(!input.isEmpty()) {
 
-        if(input.charAt(input.length()-1) == ']') {
-            input = input.substring(0, input.length()-1);
-        }
-        String[] splitedCoordsList = input.split("],");
-        for (String s : splitedCoordsList) {
-            String splitedCoordsSub = s.trim().substring(1);
-            //System.out.println(splitedCoordsSub);
-            String[] splitedCoords = splitedCoordsSub.split(",");
-            String formated = splitedCoords[1]+","+splitedCoords[0];
-            formated = formated.trim();
-            if(distanceViaLatEtLong(Double.parseDouble(splitedCoords[1]), lastPoint_lat, Double.parseDouble(splitedCoords[0]), lastPont_long) > 30) {
-                arrangedCoords.add(formated);
-                System.out.println(formated+",");
-                lastPoint_lat = Double.parseDouble(splitedCoords[1]);
-                lastPont_long = Double.parseDouble(splitedCoords[0]);
+            double lastPoint_lat = 0;
+            double lastPont_long = 0;
+
+
+            //  !!!! PARSE THE JSON TAB CONTAINING THE COORDS
+
+            //if(input.charAt(input.length()-1) == ']') {
+            //input = input.substring(1, input.length()-1);
+            //}
+
+            input = input.substring(1, input.length()-1);
+            input = input + ",";
+
+            String[] splitedCoordsList = input.split("],");
+            for (String s : splitedCoordsList) {
+                String splitedCoordsSub = s.trim().substring(1);
+                //System.out.println(splitedCoordsSub);
+                String[] splitedCoords = splitedCoordsSub.split(",");
+                String formated = splitedCoords[1]+","+splitedCoords[0];
+                formated = formated.trim();
+                //System.out.println("formated out : " + formated);
+                if(distanceViaLatEtLong(Double.parseDouble(splitedCoords[1]), lastPoint_lat, Double.parseDouble(splitedCoords[0]), lastPont_long) > 30) {
+                    arrangedCoords.add(formated);
+                    //System.out.println(formated+",");
+                    lastPoint_lat = Double.parseDouble(splitedCoords[1]);
+                    lastPont_long = Double.parseDouble(splitedCoords[0]);
+                }
             }
         }
         return arrangedCoords;
+    }
+
+    public String getOutput(String input) {
+        FormatingAndInversing(input);
+
+        String beg = "[";
+        for(String element : arrangedCoords) {
+            element = "["+element+"],";
+            beg += element;
+        }
+        beg = beg.substring(0, beg.length()-1);
+        beg += "]";
+
+        System.out.println("[*] Fetched coords: " + arrangedCoords.size());
+        arrangedCoords.clear();
+        return beg;
     }
 }
